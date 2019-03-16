c463fd2da810bb99bbfed4c7acba42b9756480c9
vnijs <radiant@rady.ucsd.edu>
2019-03-15 17:46:49 -0700

    update for: radiant, miniUI, webshot, tinytex, usethis, radiant.update, svglite (03-15-2019)

--- minicran.R
+++ minicran.R
@@ -38,9 +38,10 @@ pkgs_src <- c(
   pkgs, "gitgadget", "devtools", "roxygen2", "caret", "ranger", "randomForest",
   "gbm", "dbplyr", "DBI", "RSQLite", "RPostgreSQL", "pool", "odbc", "xgboost",
   "png", "shinydashboard", "flexdashboard", "reticulate", "styler", "caTools",
-  "tidyverse", "testthat", "tfestimators", "keras", "packrat", "sparklyr",
+  "tidyverse", "testthat", "tfestimators", "keras", "packrat", "sparklyr", "sparkxgb",
   "gganimate", "gifski", "here", "zipcode", "forcats", "future", "parsnip",
-  "lime", "rsample", "infer", "yardstick", "tidyquant", "recipes", "vip", "kableExtra"
+  "lime", "rsample", "infer", "yardstick", "tidyquant", "recipes", "vip", "kableExtra",
+  "ggraph", "tidygraph"
 )
 
 ## for khansen
@@ -51,46 +52,46 @@ pkgs_src <- c(
 )
 
 ## anything to remove?
-pkgs_rem <- c(
-  "checkmate", "magick", "matrixStats", "pander", "pryr", "rapportools",
-  "summarytools", "rstan", "greta", "rstanarm", "rstantools", "listenv",
-  "shinystan", "tidymodels", "tidyposterior", "tidypredict"
-)
+# pkgs_rem <- c(
+#   "checkmate", "magick", "matrixStats", "pander", "pryr", "rapportools",
+#   "summarytools", "rstan", "greta", "rstanarm", "rstantools", "listenv",
+#   "shinystan", "tidymodels", "tidyposterior", "tidypredict"
+# )
 
 # building minicran for source packages
 pkgList <- pkgDep(pkgs_src, repos = repos, type = "source", suggests = FALSE)
-#length(pkgList)
-#pkgList <- setdiff(pkgList, pkgs_rem)
-#length(pkgList)
+# length(pkgList)
 # pkgs_rem[pkgs_rem %in% pkgList]
+# pkgList <- setdiff(pkgList, pkgs_rem)
+# length(pkgList)
 to_rm <- selMakeRepo(pkgList, path = pth, minicran, repos = repos, type = "source")
 
 # building minicran for windows binaries
 pkgList <- pkgDep(pkgs, repos = repos, type = "win.binary", suggests = FALSE, Rversion = "3.4")
-length(pkgList)
-pkgList <- setdiff(pkgList, pkgs_rem)
-length(pkgList)
+# length(pkgList)
+# pkgList <- setdiff(pkgList, pkgs_rem)
+# length(pkgList)
 to_rm <- selMakeRepo(pkgList, path = pth, minicran, repos = repos, type = "win.binary", Rversion = "3.4")
 
 # building minicran for windows binaries
 pkgList <- pkgDep(pkgs, repos = repos, type = "win.binary", suggests = FALSE, Rversion = "3.5")
-length(pkgList)
-pkgList <- setdiff(pkgList, pkgs_rem)
-length(pkgList)
+# length(pkgList)
+# pkgList <- setdiff(pkgList, pkgs_rem)
+# length(pkgList)
 to_rm <- selMakeRepo(pkgList, path = pth, minicran, repos = repos, type = "win.binary", Rversion = "3.5")
 
 # building minicran for mac el-capitan binaries
 pkgList <- pkgDep(pkgs, repos = repos, type = "mac.binary.el-capitan", suggests = FALSE, Rversion = "3.4")
-length(pkgList)
-pkgList <- setdiff(pkgList, pkgs_rem)
-length(pkgList)
+# length(pkgList)
+# pkgList <- setdiff(pkgList, pkgs_rem)
+# length(pkgList)
 to_rm <- selMakeRepo(pkgList, path = pth, minicran, repos = repos, type = "mac.binary.el-capitan", Rversion = "3.4")
 
 # building minicran for mac el-capitan binaries
 pkgList <- pkgDep(pkgs, repos = repos, type = "mac.binary.el-capitan", suggests = FALSE, Rversion = "3.5")
-length(pkgList)
-pkgList <- setdiff(pkgList, pkgs_rem)
-length(pkgList)
+# length(pkgList)
+# pkgList <- setdiff(pkgList, pkgs_rem)
+# length(pkgList)
 cat(paste0("pkgs <- c('", paste0(pkgList, collapse = "', '"), "')"), file = "pkgs.R")
 to_rm <- selMakeRepo(pkgList, path = pth, minicran, repos = repos, type = "mac.binary.el-capitan", Rversion = "3.5")
 

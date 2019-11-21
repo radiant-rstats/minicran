9a49c25c2135b6fd5573549c451e45bf35c25bbf
vnijs <radiant@rady.ucsd.edu>
2019-11-20 22:52:52 -0800

    update for: radiant, miniUI, webshot, tinytex, usethis, radiant.update, svglite, formatR (11-20-2019)

--- minicran.R
+++ minicran.R
@@ -39,7 +39,7 @@ pkgs_src <- c(
   "forge", "gganimate", "gifski", "here", "zipcode", "forcats", "future", "parsnip",
   "lime", "rsample", "infer", "yardstick", "tidyquant", "recipes", "vip", "kableExtra",
   "ggraph", "tidygraph", "bookdown", "lintr", "languageserver", "rprojroot", "iml",
-  "xaringan", "magick", "arm", "rstan", "dtplyr", "renv"
+  "xaringan", "magick", "arm", "rstan", "dtplyr", "renv", "RSelium"
 )
 
 # for Karsten

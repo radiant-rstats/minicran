bd6f5026df12040847fa7057f959c88c1a656beb
vnijs <vnijs@ucsd.edu>
2021-03-20 08:33:32 +0000

    use rspm on server

--- rsm-msba.R
+++ rsm-msba.R
@@ -1,6 +1,6 @@
 # options(HTTPUserAgent = sprintf("R/%s R (%s)", getRversion(), paste(getRversion(), R.version$platform, R.version$arch, R.version$os)))
-# RSM = "https://rsm-compute-01.ucsd.edu:4242/rsm-msba/__linux__/focal/latest",
 repos <- c(
+  RSM = "https://rsm-compute-01.ucsd.edu:4242/rsm-msba/__linux__/focal/latest",
   RSPM = "https://packagemanager.rstudio.com/all/__linux__/focal/latest",
   RSMCRAN = "https://radiant-rstats.github.io/minicran/",
   CRAN = "https://cloud.r-project.org"

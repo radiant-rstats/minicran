0fa28bd6a4f53be28634d900e08af9343ba75545
vnijs <vnijs@ucsd.edu>
2022-07-18 11:51:32 -0700

    update for: radiant, miniUI, webshot, tinytex, usethis, svglite, ranger, xgboost, pdp, patchwork, lobstr, remotes, bslib, png, GPArotation (07-18-2022)

--- minicran.R
+++ minicran.R
@@ -31,7 +31,8 @@ pth <- rprojroot::find_root(rprojroot::has_file("README.md"))
 pkgs <- c(
   "radiant", "miniUI", "webshot", "tinytex",
   "usethis", "svglite", "ranger", "xgboost",
-  "pdp", "patchwork", "lobstr", "remotes"
+  "pdp", "patchwork", "lobstr", "remotes",
+  "bslib", "png"
 )
 
 # check only files that needed updating or adding

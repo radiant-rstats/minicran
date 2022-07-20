9f5c5fc9a0ea62e29b692f14d0b7092367a1648a
vnijs <vnijs@ucsd.edu>
2022-07-20 12:17:29 -0700

    upgrade="never"

--- install-msba-m1.R
+++ install-msba-m1.R
@@ -1,7 +1,7 @@
 ## install script for R(adiant) @ Rady School of Management (MBA)
 owd <- getwd()
 options(HTTPUserAgent = sprintf("R/%s R (%s)", getRversion(), paste(getRversion(), R.version$platform, R.version$arch, R.version$os)))
-repos = c(
+repos <- c(
   MINICRAN = "https://radiant-rstats.github.io/minicran/",
   CRAN = "https://cloud.r-project.org"
 )
@@ -11,24 +11,24 @@ build <- function(type = "binary") {
   update.packages(lib.loc = .libPaths()[1], ask = FALSE, type = type)
 
   # bml
-  install.packages(c('cmdstanr', 'posterior', 'bayesplot'), Ncpus = -1)
+  install.packages(c("cmdstanr", "posterior", "bayesplot"), Ncpus = -1)
 
   # quarto
-  install.packages('quarto', Ncpus = -1)
+  install.packages("quarto", Ncpus = -1)
 
   # tidyverse
-  install.packages(c('tidyverse', 'devtools', 'rmarkdown', 'vroom', 'gert', 'usethis'), Ncpus = -1)
-  install.packages(c('dbplyr', 'DBI', 'dtplyr', 'RPostgres', 'RSQLite', 'fst'), Ncpus = -1)
+  install.packages(c("tidyverse", "devtools", "rmarkdown", "vroom", "gert", "usethis"), Ncpus = -1)
+  install.packages(c("dbplyr", "DBI", "dtplyr", "RPostgres", "RSQLite", "fst"), Ncpus = -1)
 
   # radiant
-  install.packages(c('radiant', 'gitgadget', 'miniUI', 'webshot', 'tinytex', 'svglite', 'readxl', 'writexl'), Ncpus=-1)
-  install.packages(c('devtools', 'remotes', 'formatR', 'styler', 'reticulate', 'renv'), Ncpus=-1)
-  install.packages(c('arrow', 'duckdb', 'fs', 'janitor', 'palmerpenguins', 'stringr', 'tictoc'), Ncpus=-1)
-  install.packages(c('httpgd', 'languageserver'), Ncpus=-1)
+  install.packages(c("radiant", "gitgadget", "miniUI", "webshot", "tinytex", "svglite", "readxl", "writexl"), Ncpus = -1)
+  install.packages(c("devtools", "remotes", "formatR", "styler", "reticulate", "renv"), Ncpus = -1)
+  install.packages(c("arrow", "duckdb", "fs", "janitor", "palmerpenguins", "stringr", "tictoc"), Ncpus = -1)
+  install.packages(c("httpgd", "languageserver"), Ncpus = -1)
   remotes::install_github("radiant-rstats/radiant.update", upgrade = "never")
   remotes::install_github("radiant-rstats/radiant.data", upgrade = "never")
   remotes::install_github("vnijs/DiagrammeR", upgrade = "never")
-  remotes::install_github('vnijs/gitgadget')
+  remotes::install_github("vnijs/gitgadget", upgrade = "never")
 
   # see https://github.com/wch/webshot/issues/25#event-740360519
   if (is.null(webshot:::find_phantom())) webshot::install_phantomjs()
@@ -45,16 +45,15 @@ rv <- paste(rv$major, rv$minor, sep = ".")
 if (rv < "4.2.0") {
   cat("R 4.2.0 or later is required. Please install the latest\nversion of R from https://cloud.r-project.org/")
 } else {
-
   os <- Sys.info()["sysname"]
   if (os == "Windows") {
-    lp <- .libPaths()[grepl("Documents",.libPaths())]
+    lp <- .libPaths()[grepl("Documents", .libPaths())]
     if (grepl("(Prog)|(PROG)", Sys.getenv("R_HOME"))) {
       # rv <- paste(rv$major, rv$minor, sep = ".")
-      cat(paste0("It seems you installed R in the Program Files directory. This can\ncause problems so we recommend you uninstall R, delete the Documents/R\ndirectory on your computer, and then re-install the latest version of R.\n\nThe most convenient way to install all required tools on Windows\nis to use the all-in-one-installer linked on the page below (see\nthe section on 'Installing Radiant on Windows')\n\n https://radiant-rstats.github.io/docs/install.html" ),"\n\n")
+      cat(paste0("It seems you installed R in the Program Files directory. This can\ncause problems so we recommend you uninstall R, delete the Documents/R\ndirectory on your computer, and then re-install the latest version of R.\n\nThe most convenient way to install all required tools on Windows\nis to use the all-in-one-installer linked on the page below (see\nthe section on 'Installing Radiant on Windows')\n\n https://radiant-rstats.github.io/docs/install.html"), "\n\n")
     } else if (length(lp) > 0) {
       cat("Installing R-packages in the directory printed below often causes\nproblems on Windows. Please remove the 'Documents/R' directory\non your computer, close and restart R(studio), and then run the\ncommand below:\n\nsource(\"https://raw.githubusercontent.com/radiant-rstats/minicran/gh-pages/install.R\").\n\n")
-      cat(paste0(lp, collapse = "\n"),"\n\n")
+      cat(paste0(lp, collapse = "\n"), "\n\n")
     } else {
       build()
       install.packages("installr")
@@ -65,8 +64,8 @@ if (rv < "4.2.0") {
 
       ## get rstudio - release
       page <- readLines("https://www.rstudio.com/products/rstudio/download", warn = FALSE)
-      pat <- "//download1.rstudio.org/desktop/windows/RStudio-[0-9.]+.[0-9.]+.[0-9]+.exe";
-      URL <- paste0("https:",regmatches(page,regexpr(pat,page))[1])
+      pat <- "//download1.rstudio.org/desktop/windows/RStudio-[0-9.]+.[0-9.]+.[0-9]+.exe"
+      URL <- paste0("https:", regmatches(page, regexpr(pat, page))[1])
 
       installr::install.URL(URL, installer_option = "/S")
 
@@ -95,18 +94,16 @@ if (rv < "4.2.0") {
       cat("\n\nInstallation on Windows complete. Close R, (re)start Rstudio, and select 'Start radiant'\nfrom the Addins menu to get started\n\n")
     }
   } else if (os == "Darwin") {
-
     resp <- system("sw_vers -productVersion", intern = TRUE)
     if (as.numeric_version(resp) < as.numeric_version("10.9")) {
       cat("The version of OSX on your mac is no longer supported by R. You will need to upgrade the OS before proceeding\n\n")
     } else {
-
       build()
       ##  based on https://github.com/talgalili/installr/blob/82bf5b542ce6d2ef4ebc6359a4772e0c87427b64/R/install.R#L805-L813
       ## get rstudio - release
       page <- readLines("https://www.rstudio.com/products/rstudio/download", warn = FALSE)
-      pat <- "//download1.rstudio.org/desktop/macos/RStudio-[0-9.]+.[0-9.]+.[0-9]+.dmg";
-      URL <- paste0("https:",regmatches(page, regexpr(pat, page))[1])
+      pat <- "//download1.rstudio.org/desktop/macos/RStudio-[0-9.]+.[0-9.]+.[0-9]+.dmg"
+      URL <- paste0("https:", regmatches(page, regexpr(pat, page))[1])
 
       ## get rstudio - preview
       # page <- readLines("https://www.rstudio.com/products/rstudio/download/preview/", warn = FALSE)
@@ -114,7 +111,7 @@ if (rv < "4.2.0") {
       # URL <- paste0("https:",regmatches(page,regexpr(pat,page))[1])
 
       setwd(tempdir())
-      download.file(URL,"Rstudio.dmg")
+      download.file(URL, "Rstudio.dmg")
       system("open Rstudio.dmg")
       cat("Please drag-and-drop the Rstudio image to the Applications folder on your Mac\n")
 
@@ -151,4 +148,3 @@ if (rv < "4.2.0") {
 }
 
 setwd(owd)
-

b87aad6c5b10e60c1b4e1cb84defe7f0f38b7714
rsm-vnijs <vnijs@ucsd.edu>
2025-08-25 19:01:20 -0700

    use CRAN

--- install-minicran.R
+++ install-minicran.R
@@ -1,10 +1,15 @@
 ## install script for R(adiant) @ Rady School of Management
 owd <- getwd()
 options(HTTPUserAgent = sprintf("R/%s R (%s)", getRversion(), paste(getRversion(), R.version$platform, R.version$arch, R.version$os)))
-repos <- c(CRAN = "https://cloud.r-project.org")
+repos <- c(
+  RSM = "https://rsm-compute-01.ucsd.edu:4242/rsm-msba/__linux__/focal/latest",
+  RSPM = "https://packagemanager.rstudio.com/all/__linux__/focal/latest",
+  MINICRAN = "https://radiant-rstats.github.io/minicran/",
+  CRAN = "https://cloud.r-project.org"
+)
 
 os <- Sys.info()["sysname"]
-# repos <- if (os == "Linux") repos else repos[c(3, 4)]
+repos <- if (os == "Linux") repos else repos[c(3, 4)]
 options(repos = repos)
 
 build <- function(type = ifelse(os == "Linux", "source", "binary")) {
@@ -15,7 +20,30 @@ build <- function(type = ifelse(os == "Linux", "source", "binary")) {
     pkgs <- x[!x %in% ipkgs]
     if (length(pkgs) > 0) install.packages(pkgs, lib = .libPaths()[1], type = type)
   }
-  install(c("radiant", "miniUI", "webshot", "usethis", "remotes", "tinytex"))
+  install(c("radiant", "miniUI", "webshot", "tinytex", "usethis", "svglite", "remotes"))
+
+  if (!"radiant.update" %in% ipkgs) {
+    ap <- available.packages(repos = repos)[, "Package"]
+    if ("radiant.update" %in% ap) {
+      install.packages("radiant.update", lib = .libPaths()[1], repos = repos)
+      radiant.update::radiant.update()
+    } else {
+      ru <- try(remotes::install_github("radiant-rstats/radiant.update", upgrade = "never"))
+      if (inherits("try-error", ru)) {
+        print("radiant.update cannot be installed on your system at this time")
+      } else {
+        radiant.update::radiant.update()
+      }
+    }
+  } else {
+    radiant.update::radiant.update()
+  }
+
+  ## needed for windoze
+  # pkgs <- new.packages(lib.loc = .libPaths()[1], repos = repos[c(1, 3)], ask = FALSE)
+  # if (length(pkgs) > 0) {
+  #   install.packages(pkgs, type = type)
+  # }
 
   # see https://github.com/wch/webshot/issues/25#event-740360519
   if (is.null(webshot:::find_phantom())) webshot::install_phantomjs()
@@ -29,8 +57,8 @@ readliner <- function(text, inp = "", resp = "[yYnN]") {
 rv <- R.Version()
 rv <- paste(rv$major, rv$minor, sep = ".")
 
-if (rv < "4.2") {
-  cat("Radiant requires R-4.2.0 or later. Please install the latest\nversion of R from https://cloud.r-project.org/")
+if (rv < "3.6") {
+  cat("Radiant requires R-3.6.0 or later. Please install the latest\nversion of R from https://cloud.r-project.org/")
 } else {
   os <- Sys.info()["sysname"]
   if (os == "Windows") {
@@ -45,6 +73,7 @@ if (rv < "4.2") {
       build()
       install.packages("installr")
 
+
       ## get rstudio - release
       page <- readLines("https://posit.co/download/rstudio-desktop/", warn = FALSE)
       pat <- "//download1.rstudio.org/electron/windows/RStudio-[0-9.]+.[0-9.]+.[0-9]+-[0-9]+.exe"
@@ -56,8 +85,9 @@ if (rv < "4.2") {
       wz <- suppressWarnings(system("where R", intern = TRUE))
       w7z <- suppressWarnings(system("where 7z", intern = TRUE))
       if (!grepl("zip", wz) && !grepl("7-Zip", w7z)) {
-        # URL <- "https://www.7-zip.org/a/7z2200-x64.exe"
-        URL <- "https://www.7-zip.org/a/7z2501-x64.exe"
+        # URL <- "http://rady.ucsd.edu/faculty/directory/nijs/pub/docs/radiant/7z1604-x64.exe"
+        # URL <- "https://www.7-zip.org/a/7z1604-x64.exe"
+        URL <- "https://www.7-zip.org/a/7z2200-x64.exe"
         installr::install.URL(URL)
         if (file.exists(file.path(Sys.getenv("ProgramFiles"), "7-Zip"))) {
           ## update path
@@ -79,7 +109,6 @@ if (rv < "4.2") {
     }
   } else if (os == "Darwin") {
     resp <- system("sw_vers -productVersion", intern = TRUE)
-    resp
     if (as.numeric_version(resp) < as.numeric_version("10.9")) {
       cat("The version of OSX on your mac is no longer supported by R. You will need to upgrade the OS before proceeding\n\n")
     } else {
@@ -91,28 +120,8 @@ if (rv < "4.2") {
 
       setwd(tempdir())
       download.file(URL, "Rstudio.dmg")
-      # Mount the DMG
-      system("hdiutil attach Rstudio.dmg")
-      # Find the mounted volume name
-      vols <- list.files("/Volumes", pattern = "^RStudio", full.names = TRUE)
-      if (length(vols) > 0) {
-        rstudio_app <- file.path(vols[1], "RStudio.app")
-        # Copy to /Applications (may require admin rights)
-        res <- system(paste0("cp -R '\"", rstudio_app, "\"' /Applications/"))
-        if (res == 0) {
-          cat("RStudio.app was copied to /Applications.\n")
-          # Unmount the DMG
-          system(paste0("hdiutil detach '\"", vols[1], "\"'"))
-        } else {
-          # Fallback: open DMG and show drag-and-drop message
-          system("open Rstudio.dmg")
-          cat("Automatic copy to /Applications failed. Please drag-and-drop the Rstudio image to the Applications folder on your Mac\n")
-        }
-      } else {
-        # Fallback: open DMG and show drag-and-drop message if volume not found
-        system("open Rstudio.dmg")
-        cat("Could not find mounted RStudio volume. Please drag-and-drop the Rstudio image to the Applications folder on your Mac\n")
-      }
+      system("open Rstudio.dmg")
+      cat("Please drag-and-drop the Rstudio image to the Applications folder on your Mac\n")
 
       ## moving Rstudio.app doesn't seem to work just yet
       # rstudio <- file.path("/Volumes", list.files("/Volumes", pattern = "^RStudio-*"), "RStudio.app")
@@ -129,7 +138,20 @@ if (rv < "4.2") {
       cat("\n\nInstallation on Mac complete. Close R, (re)start Rstudio, and select 'Start radiant'\nfrom the Addins menu to get started\n\n")
     }
   } else {
-    cat("\n\nThe install script does not currently support your OS\n")
+    cat("\n\nThe install script only partially supports your OS\n")
+    cat("You may prefer to use a docker image of Radiant and related software\nSee https://github.com/radiant-rstats/docker/blob/master/install/rsm-msba-linux.md for details\n\n")
+    inp <- readliner("Do wish to proceed with the local install of Radiant and its dependencies? Press y or n and then press return: ")
+    if (grepl("[yY]", inp)) {
+      build()
+      pl <- suppressWarnings(system("which pdflatex", intern = TRUE))
+      if (length(pl) == 0) {
+        cat("To generate PDF reports in Radiant (Report > Rmd) you will need TinyTex (or LaTex).")
+        inp <- readliner("Proceed with the TinyTex download and install? Press y or n and then press return: ")
+        if (grepl("[yY]", inp)) {
+          tinytex::install_tinytex()
+        }
+      }
+    }
   }
 }
 
